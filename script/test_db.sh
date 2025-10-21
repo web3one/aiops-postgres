e23b406736fc3591c9300aa92484d1d188470ea1
jiaql <jiaql@founcer.com>
2025-10-21 14:41:13 +0800

    feat: add configuration files for Aione deployment and upgrade scripts

--- script/test_db.sh
+++ script/test_db.sh
@@ -1,1 +1,3 @@
-PGPASSWORD=aiops2088 psql -U postgres -h 127.0.0.1 -p 6432
+ kubectl -n aiops-postgres get pods -l application=spilo,cluster-name=aiops-cluster --show-labels
+
+PGPASSWORD=aiops2088 psql -U postgres -h 127.0.0.1 -p 5432
